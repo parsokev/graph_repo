571ac632444ef67eeb8b731ed2932d85b5859327
Kevin Parsons <parsokev@oregonstate.edu>
2024-07-24 19:23:16 -0700

    Added shell script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ./graph_repo
+mkdir build
+mkdir ./build/release
+mkdir ./build/debug
+cmake -B ./build -S ../graph_repo
+cd ./build
+make
+cd ./release
+cmake -DCMAKE_BUILD_TYPE=Release ../../
+make
+cd ..
+cd ./debug
+cmake -DCMAKE_BUILD_TYPE=Debug ../../
+make
+cd ../..
