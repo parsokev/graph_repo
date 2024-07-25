9e81d2b4f3c4f7af867d40d8630ba5e7f7a60ca4
Kevin Parsons <parsokev@oregonstate.edu>
2024-07-24 19:40:48 -0700

    update shell script and config build

--- setup.sh
+++ setup.sh
@@ -4,10 +4,7 @@ cd ./graph_repo
 mkdir build
 mkdir ./build/release
 mkdir ./build/debug
-cmake -B ./build -S ../graph_repo
-cd ./build
-make
-cd ./release
+cd ./build/release
 cmake -DCMAKE_BUILD_TYPE=Release ../../
 make
 cd ..
