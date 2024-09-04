f0db52ac646a796217ac450a9c6a7e680c4c5cb7
Kevin <parsokev@oregonstate.edu>
2024-09-03 20:15:52 -0700

    Updated functions for handling user input and added new functions to handle creating overlay .gv file

--- includes/graph_input.hpp
+++ includes/graph_input.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "../includes/derived_hashmap.hpp"
+#include "derived_hashmap.hpp"
 // #include "master_hashmap.hpp"
 #include "gprintf.hpp"
 
 extern int get_graph_filename(std::string& directory_name, std::string& user_file); 
 
-extern void get_graph_vertex_count(long int& vertex_count);
+extern int get_graph_vertex_count(long int& vertex_count);
 
 extern int get_shortest_path(main_hashmap<double>& main, std::string& path_filename);
 
