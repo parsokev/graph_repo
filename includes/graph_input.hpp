994ac04d0ccd025b0de4ed23d3a6c621b6b63175
Kevin <parsokev@oregonstate.edu>
2024-09-08 20:48:54 -0700

    Updated input handling functions for accepting rvalue references and additional vector args

--- includes/graph_input.hpp
+++ includes/graph_input.hpp
@@ -2,12 +2,14 @@
 
 #include "derived_hashmap.hpp"
 // #include "master_hashmap.hpp"
-#include "gprintf.hpp"
+
 
 extern int get_graph_filename(std::string& directory_name, std::string& user_file); 
 
 extern int get_graph_vertex_count(long int& vertex_count);
 
-extern int get_shortest_path(main_hashmap<double>& main, std::string& path_filename);
+extern int get_shortest_path(main_hashmap<double>&& main, std::string& path_filename);
+// extern int get_shortest_path(main_hashmap<double>& main, std::string& path_filename);
 
-extern int get_requested_algorithm (std::string& algorithm_type, main_hashmap<double>& main, std::string& path_filename);
+extern int get_requested_algorithm (std::string& algorithm_type, main_hashmap<double>&& main, std::string& path_filename);
+// extern int get_requested_algorithm (std::string& algorithm_type, main_hashmap<double>& main, std::string& path_filename);
