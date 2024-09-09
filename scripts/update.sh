78fc259440bca9dc4ab7a232703ac0e81c683ad9
Kevin <parsokev@oregonstate.edu>
2024-09-08 20:44:12 -0700

    Updated update script for users with additional links to README for error reporting output

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,6 +5,7 @@
 
 echo "NOTE: If you wish to add or remove any original files from the \"src\" or \"includes\" directories,"
 echo "you may need to delete the \"build\" directory and rerun the appropriate setup script to rebuild."
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 
 
 # Attempt to navigate to directory containing Release Configuration of CMake Project
@@ -12,6 +13,7 @@ cd ./build/release
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to navigate to the Release Directory."
 echo "Please ensure you call \"update.sh\" in current working directory of \"graph_repo\""
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 kill -INT 0
 fi
 
@@ -20,6 +22,8 @@ make
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to rebuild the Release Configuration."
 echo "Please ensure you have run the \"setup.sh\" script at least once before calling the \"update.sh\" script"
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
+
 cd ../../
 kill -INT 0
 fi
@@ -32,6 +36,7 @@ cd ./build/debug
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to navigate to the Source Directory."
 echo "Please ensure you have Debug Configuration is located at \"./graph_repo/build/debug\""
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 kill -INT 0
 fi
 
@@ -40,6 +45,7 @@ make
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to rebuild the Debug Configuration."
 echo "Please ensure you have run the \"setup.sh\" script at least once before calling the \"update.sh\" script"
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 cd ../../
 kill -INT 0
 fi
@@ -48,3 +54,4 @@ fi
 cd ../../
 
 echo "Release and Debug Configurations Have Been Successfully Rebuilt!"
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
