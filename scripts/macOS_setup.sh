cdcd5fb9962fc37f532e93419509776f94ea212b
Kevin <parsokev@oregonstate.edu>
2024-09-08 20:43:03 -0700

    Updated setup script for macOS users with additional links to README for error reporting output

--- scripts/macOS_setup.sh
+++ scripts/macOS_setup.sh
@@ -5,6 +5,7 @@ echo "Checking for updates to local version of Homebrew..."
 brew update && brew upgrade
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to update brew... please visit https://brew.sh/ for troubleshooting."
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 kill -INT 0
 fi
 
@@ -15,6 +16,7 @@ if [ $? -ne 0 ]; then
     echo "Error: Failed to install all package dependencies listed in \"requirements.txt\""
     echo "Please ensure you have not moved \"requirements.txt\" from its original position within the cloned repository"
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 fi
 
@@ -24,6 +26,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to change to \"graph_repo\" directory from current working directory of ${PWD}."
     echo "Please ensure bash script is called at current working directory one level above directory containing git repository (level at which git clone was called)"
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 fi
 
@@ -33,6 +36,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to create \"build\" directory in cloned repo directory."
     echo "Aborting setup process..."
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 else
     echo "\"build\" directory successfully created."
@@ -44,6 +48,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to create \"build/release/\" subdirectory in cloned repo directory."
     echo "Aborting setup process.."
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 else
     echo "\"release\" subdirectory successfully created."
@@ -55,6 +60,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to create \"build/debug/\" subdirectory in cloned repo directory."
     echo "Aborting setup process.."
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 else
     echo "\"debug\" subdirectory successfully created."
@@ -70,6 +76,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to create CMake \"Release\" Configuaration in \"build/release/\" subdirectory in cloned repo directory."
     echo "Please ensure you are calling script from top level directory. Aborting setup process.."
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script according to README instructions"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 else
     echo "Attempting to build \"Release\" configuration using generated CMakefile..."
@@ -81,6 +88,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to build CMake \"Release\" Configuaration in \"build/release/\" subdirectory in cloned repo directory."
     echo "Please ensure you are calling script from top level directory. Aborting setup process.."
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script according to README instructions"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 else
     echo "\"Release\" Configuration of CMake Project successfully built!"
@@ -95,6 +103,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to create CMake \"Debug\" Configuaration in \"build/debug/\" subdirectory in cloned repo directory."
     echo "Please ensure you are calling script from top level directory. Aborting setup process.."
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script according to README instructions"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 else
 echo "Attempting to build \"Debug\" configuration using generated CMakefile..."
@@ -106,6 +115,7 @@ if [ $? -ne 0 ]; then
     echo "Failed to build CMake \"Debug\" Configuaration in \"build/debug/\" subdirectory in cloned repo directory."
     echo "Please ensure you are calling script from top level directory. Aborting setup process.."
     echo "If you wish to retry at any time, simply delete the \"build\" directory and re-run setup script according to README instructions"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     kill -INT 0
 else
     echo "\"Debug\" Configuration of CMake Project successfully built!"
@@ -120,3 +130,4 @@ echo "To Run the Program in Release Mode, Enter into the Terminal: \"./build/rel
 echo "To Run the Program in Debug Mode, Enter into the Terminal: \"./build/debug/Graph_TIProject\""
 echo "EXECUTING TESTING SUITE:"
 echo "To Run all tests within GoogleTest testing suite, Enter: \"./build/debug/tests\""
+echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
