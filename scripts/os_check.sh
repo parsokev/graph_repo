ddc781fc01937d419060ef5c9957c9de034f84b8
Kevin <parsokev@oregonstate.edu>
2024-09-08 20:43:37 -0700

    Updated os check scripts for users with additional links to README for error reporting output

--- scripts/os_check.sh
+++ scripts/os_check.sh
@@ -20,6 +20,7 @@ esac
 if [[ "$Platform" == 'Linux' ]]; then
     echo "Linux or WSL detected"
     echo "Run the \"linux_setup.sh\" script to build the CMake Project"
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 
 # If MacOSX, attempt to update Homebrew and install dependencies using Homebrew
 elif [[ "$Platform" == 'MAC_OS_X' ]]; then
@@ -29,6 +30,7 @@ elif [[ "$Platform" == 'MAC_OS_X' ]]; then
     brew --version
     if [ $? -ne 0 ]; then 
     echo "Homebrew not detected on local machine... please visit https://brew.sh/ to have Homebrew installed."
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
     fi
 
 # If Windows is detected, notify of script incompatibility
@@ -36,8 +38,10 @@ elif [[ "$Platform" == 'Windows' ]]; then
     echo "Windows OS detected with bash environment. Please ensure you have WSL installed to run this script in a Bash shell."
     echo "The \"linux_setup.sh\" script is not intended to run in Cygwin/MingW due to varying environment file paths"
     echo "Please visit https://learn.microsoft.com/en-us/windows/wsl/install to setup WSL, which is supported by \"linux_setup.sh\""
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 
 # If OS is not recognized, exit with notification
 else
     echo "Unrecognized distribution. If running in Linux environment other than Debian or Ubuntu, this script is currently unable to run in these environments."
+    echo "Please view the README for further information on feature access and troubleshooting: https://github.com/parsokev/graph_repo"
 fi
