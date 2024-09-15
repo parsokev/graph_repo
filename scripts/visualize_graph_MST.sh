191ccd8813b548bfe25afc74388508f5bff8daa3
Kevin <parsokev@oregonstate.edu>
2024-09-15 15:47:52 -0700

    Updated dot commands in visualization scripts to improve appearance of graphical image output

--- scripts/visualize_graph_MST.sh
+++ scripts/visualize_graph_MST.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-dot -Tpng ./dot_graphs/full_graph.gv -o ./graph_images/full_graph.png
+dot -Tpng:cairo ./dot_graphs/full_graph.gv -o ./graph_images/full_graph.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"full_graph.png\"."
 fi
 
 
-dot -Tpng ./dot_graphs/MST_overlay.gv -o ./graph_images/MST_overlay.png
+dot -Tpng:cairo ./dot_graphs/MST_overlay.gv -o ./graph_images/MST_overlay.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"MST_overlay.png\"."
 fi
