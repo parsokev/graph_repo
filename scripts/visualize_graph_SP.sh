04ab5acc087bba6430b3eba1d992885f0c02771c
Kevin <parsokev@oregonstate.edu>
2024-09-14 21:19:03 -0700

    Updated handling process interruption in graph output to .png format

--- scripts/visualize_graph_SP.sh
+++ scripts/visualize_graph_SP.sh
@@ -3,12 +3,10 @@
 dot -Tpng ./dot_graphs/full_graph.gv -o ./graph_images/full_graph.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"full_graph.png\"."
-kill -INT 0
 fi
 
 
 dot -Tpng ./dot_graphs/shortest_path_overlay.gv -o ./graph_images/shortest_path_overlay.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"short_path_overlay.png\"."
-kill -INT 0
 fi
