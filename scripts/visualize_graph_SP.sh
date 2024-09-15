d0cce82c31326ff8323d60607731d5eec8cc6a35
Kevin <parsokev@oregonstate.edu>
2024-09-15 15:48:13 -0700

    Updated dot commands in visualization scripts to improve appearance of graphical image output

--- scripts/visualize_graph_SP.sh
+++ scripts/visualize_graph_SP.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-dot -Tpng ./dot_graphs/full_graph.gv -o ./graph_images/full_graph.png
+dot -Tpng:cairo ./dot_graphs/full_graph.gv -o ./graph_images/full_graph.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"full_graph.png\"."
 fi
 
 
-dot -Tpng ./dot_graphs/shortest_path_overlay.gv -o ./graph_images/shortest_path_overlay.png
+dot -Tpng:cairo ./dot_graphs/shortest_path_overlay.gv -o ./graph_images/shortest_path_overlay.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"short_path_overlay.png\"."
 fi
