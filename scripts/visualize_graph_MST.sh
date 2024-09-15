16127957c5acea2a8cd8c2c11b6337f25bf94824
Kevin <parsokev@oregonstate.edu>
2024-09-14 21:18:31 -0700

    Updated handling process interruption in graph output to .png format

--- scripts/visualize_graph_MST.sh
+++ scripts/visualize_graph_MST.sh
@@ -3,12 +3,10 @@
 dot -Tpng ./dot_graphs/full_graph.gv -o ./graph_images/full_graph.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"full_graph.png\"."
-kill -INT 0
 fi
 
 
 dot -Tpng ./dot_graphs/MST_overlay.gv -o ./graph_images/MST_overlay.png
 if [ $? -ne 0 ]; then 
 echo "Error encountered while attempting to build \"MST_overlay.png\"."
-kill -INT 0
 fi
