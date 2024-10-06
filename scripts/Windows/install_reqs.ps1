de607c9c361313526e804d9635106f0672c4316b
Kevin Parsons <parsokev@oregonstate.edu>
2024-10-05 21:26:51 -0700

    Updated powershell script for handling installing package dependencies to properly recognize errors

--- scripts/Windows/install_reqs.ps1
+++ scripts/Windows/install_reqs.ps1
@@ -1,13 +1,15 @@
 $no_errors = $True
 # Install Package Dependencies
 $package_list = Get-Content -Path ./graph_repo/windows_requirements.txt
+$is_installed = $null
 foreach( $package in $package_list ) {
     winget install -e --id $package
-    if ( $? -ne 1 ) {
-        Write-Output "  ERROR: winget failed to properly install $path"
-        Write-Output "  Please check printed error messages from winget above"
+    # Ensure winget properly installed package on local machine
+    $is_installed = winget list -e --id $package
+    if ( $null -eq $is_installed ) {
+        Write-Output "  $package was unable to be properly installed with winget"
+        Write-Output "  Please ensure you have 'winget' installed are calling script at directory level above graph_repo directory"
         $no_errors = $False
-        break;
     }
     # Fix for non-winget users:
     # Install-Package -Force -Scope CurrentUser -Name $package
@@ -15,6 +17,7 @@ foreach( $package in $package_list ) {
 
 # Print instructions to User for Setting Environment Paths for Packages executed within Powershell scripts
 if ($no_errors) {
+    Write-Output ""
     Write-Output "  Installation of all Package Dependencies Successful!"
     Write-Output "  Please Add Graphviz and Cmake to your system PATH to allow scripts to build/rebuild program"
     Write-Output ""
@@ -29,7 +32,9 @@ if ($no_errors) {
     Write-Output ""
     Write-Output "  After completing the above steps, enter the following line into the Powershell terminal to complete the project setup:"
     Write-Output "          ./graph_repo/scripts/Windows/windows_setup.ps1"
+    Write-Output ""
 } else {
+    Write-Output ""
     Write-Output "  Errors were encountered while attempting to install package dependencies using 'winget'"
     Write-Output "  Please ensure your Windows OS is compatible with and has 'winget' installed"
 }
